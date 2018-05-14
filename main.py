0c6bebfc7ed5ea85d9b99862d79735ac2280b3e1
KV <kirivie@outlook.com>
2018-05-13 20:31:16 -0400

    Coordinates saved in usable lat/lon format. Added screen name field. Non-retweets no longer truncated.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from tweepy.streaming import StreamListener
 from apiconfig import *
 
 #Collects tweets with selected keyword & outputs basic info to csv file
-
 def main():
 
     #Set up twitter api auth
@@ -27,13 +26,12 @@ def main():
             print("Not an integer")
         else:
             break
-    geotagged = input("Only save tweets with coordinates? y/n: ")
-    while(geotagged.lower() not in ['y', 'n', 'yes', 'no']):
+    geotagged = input("Only save tweets with coordinates? y/n: ").lower()
+    while(geotagged not in ['y', 'n', 'yes', 'no']):
         geotagged = input("Please choose y/n: ")
-    if geotagged == 'y':
+    needCoords = False
+    if geotagged == 'y' or geotagged == 'yes':
         needCoords = True
-    else:
-        needCoords = False
 
     #Set up listener
     class TweepyListener(tweepy.StreamListener):
@@ -44,6 +42,7 @@ def main():
             #print(status.text)
             print('')
 
+        #Rate limit error
         def on_error(self, status_code):
             if status_code == 420:
                 # returning False in on_data disconnects the stream
@@ -53,28 +52,51 @@ def main():
         #Include coordinates if available.
         def on_data(self, raw_data):
             if TweepyListener.counter < int(tweetcount):
+                #loading data
                 json_data = json.loads(raw_data)
                 coords = None
+                content = ''
+                created = ''
+                retweet = ' '
+                username = ''
+
+                if 'user' in json_data:
+                    username = json_data['user']['screen_name']
                 if 'text' in json_data:
                     content = json_data['text']
+
+                if 'extended_tweet' in json_data:
+                    extended = json_data['extended_tweet']
+                    content = extended['full_text']
+
+                if 'retweeted_status' in json_data:
+                    retweet_data = json_data['retweeted_status']
+                    retweet = "retweet"
+                    content = retweet_data['text']
+
                 if 'created_at' in json_data:
                     created = json_data['created_at']
                 if 'coordinates' in json_data:
                     coords = json_data['coordinates']
+
+                #writing to file
                 if  not needCoords or needCoords and coords is not None:
-                    #print(coords)
                     print(content)
                     with open('%s.csv' % filename, 'a', encoding='utf-8-sig', newline='') as file:
                         writer = csv.writer(file)
-                        writer.writerow([created,content,coords])
-                        #writer.writerow([coords])
+                        if coords is not None:
+                            lat = coords['coordinates'][1]
+                            lon = coords['coordinates'][0]
+                            writer.writerow([username,created,content,lat,lon,retweet])
+                        else:
+                            writer.writerow([username,created,content,retweet])
                     TweepyListener.counter += 1
             else:
                 print('done! *-* \nSaved ' + str(TweepyListener.counter) + ' tweets to ' + '%s.csv' % filename)
                 return False
 
     tweepyListener = TweepyListener()
-    myStream = tweepy.Stream(auth=api.auth, listener=TweepyListener())
+    myStream = tweepy.Stream(auth=api.auth, listener=TweepyListener(), tweet_mode='extended')
     myStream.filter(track=[searchterm],async=True)
 
     print('processing *~*')
